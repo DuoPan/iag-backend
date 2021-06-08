e52f9e5683cbc8f01995c6e9fd9351f60234afc3
Duo Pan <pd54007@gmail.com>
2021-06-08 19:55:28 +1000

    Support multiple answer set

--- index.js
+++ index.js
@@ -7,18 +7,22 @@ const port = 8000;
 app.use(cors());
 app.use(express.json());
 
-let correctAnswer = '';
-let hint = '';
+let correctAnswerObj = {};
 
 const generatePassword = () => {
   const charList = [0,1,2,3,4,5,6,7,8,9];
   let password = _.shuffle(charList).slice(0,8);
-  correctAnswer = password.join('');
-  hint = password.sort().join('');
-  console.log(correctAnswer)
+  let correctAnswer = password.join('');
+  let hint = password.sort().join('');
+  if (!correctAnswerObj[hint]) {
+    correctAnswerObj[hint] = correctAnswer;
+  }
+  console.log(correctAnswerObj);
+  return hint;
 };
 
-const verifyPassword = (answer) => {
+const verifyPassword = (hint, answer) => {
+  let correctAnswer = correctAnswerObj[hint]
   let highlight = [];
   const correctAnswerArray = correctAnswer.split('');
   const answerArray = answer.split('');
@@ -38,14 +42,17 @@ app.get('/', (req, res) => {
 });
 
 app.get('/new-password', (req, res) => {
-  generatePassword();
   res.setHeader('Content-Type', 'application/json');
-  res.send(JSON.stringify({hint: hint}));
+  res.send(JSON.stringify({hint: generatePassword()}));
 });
 
 app.post('/verify-password', (req, res) => {
+  if (!correctAnswerObj[req.body.hint]) {
+    res.status(404).send({message: 'Hint Not found'});
+    return;
+  }
   res.setHeader('Content-Type', 'application/json');
-  res.send(JSON.stringify(verifyPassword(req.body.answer)));
+  res.send(JSON.stringify(verifyPassword(req.body.hint, req.body.answer)));
 });
 
 app.listen(port, () => {
